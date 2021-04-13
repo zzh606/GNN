9ae98917d118c0ae8579addf1785b29169495379
zzh606 <zehuan.zheng@foxmail.com>
2021-04-13 22:52:44 +0800

    GNN

---
+++ GNN-learn/main.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import torch_geometric.nn as pyg_nn
+import torch_geometric.utils as pyg_utils
+
+import time
+from datetime import datetime
+
+import networkx as nx
+import numpy as np
+import torch
+import torch.optim as optim
+
+from torch_geometric.datasets import TUDataset
+from torch_geometric.datasets import Planetoid
+from torch_geometric.data import DataLoader
+
+import torch_geometric.transforms as T
+
+from tensorboardX import SummaryWriter
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+
+print('dd')
